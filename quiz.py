5d0747628d8be0a6432575e6c9b3f462bc9941c6
magmagcup <47655424+magmagcup@users.noreply.github.com>
2019-09-26 17:38:43 +0700

    Add quiz

---
+++ quiz.py
@@ -0,0 +1,59 @@
+def only_middle(text):
+    """
+    Use to get the middle alphabet of a string
+    #doesn't work with empty string
+
+    :param text: (str)
+    :return: (str) string which only have the middle text
+
+    >>> only_middle("abc")
+    "b"
+    >>> only_middle("abcd")
+    "bc"
+
+    """
+    pass
+
+
+def get_number(text):
+    """
+    Delete all alphabet out from text
+
+    :param text:(str) A text which contain integers and alphabets
+    :return:(str) text that only contain integers
+
+    >>> get_number('asdav256tdhet5qs4')
+    '25654'
+    >>> get_number('4jdfwrg5jfawqr6j1afjdargae3')
+    '45613'
+    """
+    pass
+
+
+def low_to_high(number):
+    """
+    Sort number from lowest to highest in from of integer
+    :param number:(int)
+    :return:(int) start from lowest to highest:
+
+    >>> low_to_high(4321)
+    1234
+
+    >>> low_to_high(13421)
+    11234
+
+    >>> low_to_high(4242)
+    2244
+
+    >>> low_to_high(2304)
+    234
+
+    >>> low_to_high(0000)
+    0
+
+    """
+    pass
+
+
+
+
