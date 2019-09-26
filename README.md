8931fcc40fafdb1806e388fc21b49cd41d4e09f0
magmagcup <47655424+magmagcup@users.noreply.github.com>
2019-09-26 10:04:31 +0700

    Add read me, discription in teach.py

---
+++ README.md
@@ -0,0 +1,25 @@
+This is an example for my question.md on github :https://github.com/magmagcup/somequiz/blob/master/teach.py
+
+## Approach
+
+* My approach:
+For this question I store the data as a string in a list, Reason?. It look less confuser than list in list!
+
+
+* Another approach #1:
+1 string for all data.
+
+* Another approach #2:
+Store data as list in list.
+
+* Another approach #3:
+Oop
+
+It have much *much* more approach than what I have mention above so I encourage you to try thinking for other approach
+that I didn't not mention.
+
+# Tip of the day
+If your input have fix value use it, like for example in this question
+We know that first input MUST have length = 4
+
+So we could use that value as one of the condition to check if the input is correct or not
--- teach.py
+++ teach.py
@@ -1,3 +1,26 @@
+"""
+This is an example for my question.md on github :https://github.com/magmagcup/somequiz/blob/master/teach.py
+My approach:
+For this question I store the data as a string in a list, Reason?. It look less confuser than list in list!
+
+# Tip of the day
+If your input have fix value use it, like for example in this question
+We know that first input MUST have length = 4
+So we could use that value as one of the condition to check if the input is correct or not
+
+Another approach #1:
+1 string for all data.
+
+Another approach #2:
+Store data as list in list.
+
+Another approach #3:
+Oop
+
+It have much more approach than what I have mention above so I encourage you to try thinking for other approach.
+"""
+
+
 def print_data(student):
     print(f"ID: {student[0:4]}")
     print(f"Name: {student[4:-4]}")
